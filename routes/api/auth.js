d63fab0c49155e73ac06428f407cafbe11c9e26d
Ihor <ihorrykun@gmail.com>
2023-05-01 15:25:18 +0300

    add bcrypt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const Users = require("../../models/users");
+const HttpError = require("../../helpers/httpEror");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
   try {
-    const newUsers = await Users.create(req.body);
+    const { email, password } = req.body;
+    const user = await Users.findOne({ email });
+    if (user) {
+      throw HttpError(409, "Email already in use");
+    }
+    const hasPassword = await bcrypt.hash(password, 10);
+
+    const newUsers = await Users.create({ ...req.body, password: hasPassword });
     res.status(201).json({
       email: newUsers.email,
       password: newUsers.password
